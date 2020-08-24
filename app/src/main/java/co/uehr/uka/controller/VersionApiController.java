e1744df02382ebd59fd3d415e4151eb7b2afa209
uehr <uehara.yudai@gmail.com>
2020-08-24 20:23:02 +0900

    update: data save debug

--- app/src/main/java/co/uehr/uka/controller/VersionApiController.java
+++ app/src/main/java/co/uehr/uka/controller/VersionApiController.java
@@ -6,9 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import co.uehr.uka.api.VersionApi;
+import co.uehr.uka.entity.User;
+import co.uehr.uka.repository.UserRepository;
 
 @Controller
 @RequestMapping("/api/v1")
@@ -16,8 +19,15 @@ public class VersionApiController implements VersionApi{
     @Value("${uka.version}")
     private String version;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public ResponseEntity<String> getVersion() {
-        return new ResponseEntity<>(version, HttpStatus.OK);
+        User user = new User();
+        user.setName("hoge name");
+        user.setEmail("hoge email");
+        userRepository.save(user);
+        return new ResponseEntity<>(user.getId().toString(), HttpStatus.OK);
     }
 }
