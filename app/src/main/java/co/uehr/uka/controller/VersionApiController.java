70aa4501e608245dcc27c65fd9c376c95e36f8d7
uehr <uehara.yudai@gmail.com>
2020-08-30 14:54:07 +0900

    disabl: version prefix

--- app/src/main/java/co/uehr/uka/controller/VersionApiController.java
+++ app/src/main/java/co/uehr/uka/controller/VersionApiController.java
@@ -6,12 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import co.uehr.uka.api.VersionApi;
-import co.uehr.uka.entity.User;
-import co.uehr.uka.repository.UserRepository;
 
 @Controller
 @RequestMapping("/api/v1")
@@ -19,15 +16,8 @@ public class VersionApiController implements VersionApi{
     @Value("${uka.version}")
     private String version;
 
-    @Autowired
-    UserRepository userRepository;
-
     @Override
     public ResponseEntity<String> getVersion() {
-        User user = new User();
-        user.setName("hoge name");
-        user.setEmail("hoge email");
-        userRepository.save(user);
-        return new ResponseEntity<>(user.getId().toString(), HttpStatus.OK);
+        return new ResponseEntity<>(version, HttpStatus.OK);
     }
 }
