da3f0103cf499f89043a7efa7263dd9e92411d54
uehr <uehara.yudai@gmail.com>
2020-08-31 13:56:39 +0900

    debug: callback時のurlを指定

--- app/src/main/java/co/uehr/uka/controller/SignupController.java
+++ app/src/main/java/co/uehr/uka/controller/SignupController.java
@@ -1,18 +1,32 @@
 package co.uehr.uka.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
+
 import co.uehr.uka.security.AuthUtil;
 
 @Controller
 public class SignupController {
     private final ProviderSignInUtils signInUtils;
+    protected static final Logger log = LoggerFactory.getLogger(AuthUtil.class);
+
+    @Bean
+    public ProviderSignInController providerSignInController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository usersConnectionRepository, SignInAdapter signInAdapter) {
+        ProviderSignInController providerSignInController = new ProviderSignInController(connectionFactoryLocator, usersConnectionRepository, signInAdapter);
+        providerSignInController.setApplicationUrl("https://uka-api-staging.uehr.co");
+        return providerSignInController;
+    }
 
     @Autowired
     public SignupController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository connectionRepository) {
@@ -20,12 +34,12 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/signup")
-    public String signup(WebRequest request) {
+    public void signup(WebRequest request) {
         Connection<?> connection = signInUtils.getConnectionFromSession(request);
         if (connection != null) {
             AuthUtil.authenticate(connection);
             signInUtils.doPostSignUp(connection.getDisplayName(), request);
         }
-        return "redirect:/";
+        // return "redirect:/";
     }
 }
