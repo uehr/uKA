558c234a8e273d7e90f538afcf8ae860e815a28e
uehr <uehara.yudai@gmail.com>
2020-09-02 04:14:30 +0900

    debug: test when push

---
+++ app/src/test/java/co/uehr/uka/UkaApplicationTests.java
@@ -0,0 +1,11 @@
+package co.uehr.uka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UkaApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
